2cf178bb6db8e81c663f8ac6ec50e53f6f82b75b
denisanoshin <mr.denis.anoshin@gmail.com>
2025-05-13 22:44:23 +0300

    fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2781072438"
+    serviceWorkerVersion: "3164253564"
   }
 });
