cc3fbe25c5f05db3859119010e47026b68639bb2
DenisAnoshin <cweic81@gmail.com>
2025-06-10 13:44:33 +0300

    deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1040222515"
+    serviceWorkerVersion: "1541866110"
   }
 });
